ef5f006edc6f4595225eec6c62bcaaf44bdea20e
Akiko-Dev <akikowebsite@tutamail.com>
2024-06-19 21:28:37 +0000

    Basic Layout

---
+++ my-app/components/SelectionScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+const SelectionScreen = () => {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Text>Gif</Text>
+      </View>
+    );
+  };
+  
+  export default SelectionScreen;
